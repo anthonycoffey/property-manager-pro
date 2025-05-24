7300c954e8e80717db9c1e4e05eb19c8adffa22a
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-05-24 07:53:36 -0500

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  console.log(`Current mode: ${mode}`);
   return {
     plugins: [react()],
   };
